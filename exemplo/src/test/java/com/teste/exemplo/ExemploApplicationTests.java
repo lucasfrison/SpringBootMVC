e2b01c06938676507d0ae32f08dc6c4630c15606
lucasfrison <64698867+lucasfrison@users.noreply.github.com>
2022-07-06 08:53:25 -0300

    Add files via upload

---
+++ exemplo/src/test/java/com/teste/exemplo/ExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.exemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
